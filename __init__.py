f01ab5c36c1f472f7ed2f29b1befa7c2705d3f18
bdoms <ravenize@gmail.com>
2015-05-07 13:08:27 -0500

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,106 @@
+import json
+from urllib import urlencode
+from urllib2 import HTTPError, Request, urlopen
+import sys
+
+
+# API example: https://github.com/sarumont/py-trello/blob/master/trello/__init__.py
+class Trello(object):
+
+    def __init__(self, api_key, oauth_token, board_id, list_id):
+        self.api_key = api_key
+        self.oauth_token = oauth_token
+        self.board_id = board_id
+        self.list_id = list_id
+        self.base_url = 'https://api.trello.com/1'
+
+    def getLists(self):
+        return self.makeRequest('GET', '/boards/' + self.board_id + '/lists')
+
+    def getList(self, list_id):
+        return self.makeRequest('GET', '/lists/' + list_id)
+
+    def findList(self, name):
+        tlist = None
+        lists = self.getLists()
+        for l in lists:
+            if l['name'] == name:
+                tlist = l
+                break
+        return tlist
+
+    def createList(self, name):
+        new_list = self.findList(name)
+        if not new_list:
+            # NOTE that because of Trello's weird positioning mechanics
+            #      this may not always put the new list just to the right of the old one, but it'll be close
+            done_list = self.getList(self.list_id)
+            params = {'name': name, 'pos': done_list['pos'] + 1}
+            new_list = self.makeRequest('POST', '/boards/' + self.board_id + '/lists', params=params)
+        return new_list
+
+    def getCard(self, card_number):
+        return self.makeRequest('GET', '/boards/' + self.board_id + '/cards/' + card_number)
+
+    def addComment(self, card, comment):
+        params = {'text': comment}
+        return self.makeRequest('POST', '/cards/' + card['id'] + '/actions/comments', params=params)
+
+    def getCards(self):
+        return self.makeRequest('GET', '/lists/' + self.list_id + '/cards')
+
+    def moveCard(self, card, list_id, pos=None):
+        params = {'idList': list_id}
+        if pos:
+            params['pos'] = pos
+        self.makeRequest('PUT', '/cards/' + card['id'], params=params)
+
+    def moveCards(self, to_list_id):
+        cards = self.getCards()
+        for card in cards:
+            self.moveCard(card, to_list_id, pos=card['pos'])
+
+    def makeRequest(self, method, path, params=None):
+        if not params:
+            params = {}
+        params['key'] = self.api_key
+        params['token'] = self.oauth_token
+        
+        url = self.base_url + path
+        data = None
+
+        if method == 'GET':
+            url += '?' + urlencode(params)
+        elif method in ['POST', 'PUT']:
+            data = urlencode(params)
+
+        request = Request(url)
+        if method == 'PUT':
+            request.get_method = lambda: method
+
+        try:
+            if data:
+                response = urlopen(request, data=data)
+            else:
+                response = urlopen(request)
+        except HTTPError as e:
+            print e
+            print e.read()
+            result = None
+        else:
+            result = json.loads(response.read())
+
+        return result
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print 'Wrong number of arguments. Need api_key, oauth_token, board_id and list_name.'
+        sys.exit()
+    filename, api_key, oauth_token, board_id, list_name = sys.argv
+    client = Trello(api_key, oauth_token, board_id)
+    tlist = client.findList(list_name)
+    if tlist:
+        print 'List ID: ' + tlist['id']
+    else:
+        print 'List not found.'
---
+++ readme.md
@@ -0,0 +1,59 @@
+Copyright &copy; 2015, [Brendan Doms](http://www.bdoms.com/)  
+Licensed under the [MIT license](http://www.opensource.org/licenses/MIT)  
+
+
+# Trello
+
+A Python interface to the Trello API.
+
+
+## Existing Solutions
+
+There are a lot of other Python libraries out there to work with the Trello API,
+but I've found that pretty much all of them have one of these two issues:
+
+ * __They simply don't work.__ Several of the frameworks I tried just failed mysteriously.
+These projects might not have adapted to changes by Trello, or maybe they based their calls entirely on the docs
+instead of actually trying them in real life. The docs themselves are confusing, and sometimes flat out wrong themselves,
+which means that if you don't actually try a request even though the docs say it should work, it still might not.
+ * __They rely on third-party libraries.__ The Trello API is a simple REST API that returns JSON.
+Authentication is adding two parameters. There's really no reason to need anything
+beyond a few lines of the Python standard library to make those requests and parse the responses.
+
+
+## Setup
+
+You need to gather a few variables in order to properly interact with the API.
+
+### `api_key`
+
+This is found on [the Trello website](https://trello.com/app-key)
+
+### `board_name` and `board_id`
+
+The name and ID for the board you want to access are found in the board URL, which is formatted like:
+
+```
+https://trello.com/b/[BOARD_ID]/[BOARD_NAME]
+```
+
+### `oauth_token`
+
+Now that you have the required info you need to generate a token
+by going to a URL in your browser that includes your `api_key` and `board_name`:
+
+```
+https://trello.com/1/authorize?response_type=token&scope=read,write&expiration=never&key=[API_KEY]&name=[BOARD_NAME]
+```
+
+### `list_id`
+
+It is surprisingly hard to find a list's ID via the Trello interface,
+but easy to get via the API, so I built a helper function.
+To get the ID, simply call the provided `trello.py` file from the command line with the following arguments:
+
+```bash
+python trello.py api_key oauth_token board_id list_name
+```
+
+Note that `list_name` is the case sensitive display name at the top of the list.
